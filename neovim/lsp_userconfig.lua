4ce00c59a71fdbe7fc01b339ccc57498d906cca6
dcai <d@tux.im>
2023-02-21 23:57:09 +1100

    nvim: lsp keymap

--- neovim/lsp_userconfig.lua
+++ neovim/lsp_userconfig.lua
@@ -96,7 +96,7 @@ local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
 local commonBufKeyMap = function(client, bufnr)
   local bufopts = { noremap = true, silent = true, buffer = bufnr }
-  vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+  vim.keymap.set('n', 'D', vim.lsp.buf.hover, bufopts)
   vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, bufopts)
   vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, bufopts)
   vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, bufopts)
