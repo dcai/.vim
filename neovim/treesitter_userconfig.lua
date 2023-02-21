10f81185e89528e9056f6ddfd874f5a391f28ce0
dcai <d@tux.im>
2023-02-21 13:02:16 +1100

    nvim: treesitter config

--- neovim/treesitter_userconfig.lua
+++ neovim/treesitter_userconfig.lua
@@ -1,37 +1,37 @@
-require "nvim-treesitter.configs".setup {
-    highlight = {
-        enable = true,
-        disable = {}
-    },
-    indent = {
-        enable = false,
-        disable = {}
-    },
-    auth_install = true,
-    ensure_installed = {
-        "bash",
-        "css",
-        "diff",
-        "dockerfile",
-        "fish",
-        "graphql",
-        "html",
-        "javascript",
-        "json",
-        "json5",
-        "lua",
-        "make",
-        "markdown",
-        "markdown_inline",
-        "org",
-        "terraform",
-        "toml",
-        "tsx",
-        "twig",
-        "typescript",
-        "vim",
-        "yaml"
-    }
-}
-local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
-parser_config.tsx.filetype_to_parsername = {"javascript", "typescript.tsx"}
+require('nvim-treesitter.configs').setup({
+  highlight = {
+    enable = true,
+    disable = {},
+  },
+  indent = {
+    enable = false,
+    disable = {},
+  },
+  auth_install = true,
+  ensure_installed = {
+    'bash',
+    'css',
+    'diff',
+    'dockerfile',
+    'fish',
+    'graphql',
+    'html',
+    'javascript',
+    'json',
+    'json5',
+    'lua',
+    'make',
+    'markdown',
+    'markdown_inline',
+    'org',
+    'terraform',
+    'toml',
+    'tsx',
+    'twig',
+    'typescript',
+    'vim',
+    'yaml',
+  },
+})
+local parser_config = require('nvim-treesitter.parsers').get_parser_configs()
+parser_config.tsx.filetype_to_parsername = { 'javascript', 'typescript.tsx' }
