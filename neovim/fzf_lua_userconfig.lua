8f6d95dd8be2b0a97c8f9d928071541df76c4efb
dcai <d@tux.im>
2023-02-21 23:42:14 +1100

    nvim: fzf-lua config

--- neovim/fzf_lua_userconfig.lua
+++ neovim/fzf_lua_userconfig.lua
@@ -3,6 +3,46 @@ local status, fzflua = pcall(require, 'fzf-lua')
 if not status then
   return
 end
+
 fzflua.setup({
-  winopts = {},
+  winopts = {
+    width = 1,
+    row = 1,
+    height = 0.5,
+    hl = {
+      normal = 'FzfLuaNormal', -- window normal color (fg+bg)
+      border = 'FloatBorder', -- border color
+      help_normal = 'Normal', -- <F1> window normal
+      help_border = 'FloatBorder', -- <F1> window border
+      -- Only used with the builtin previewer:
+      cursor = 'Cursor', -- cursor highlight (grep/LSP matches)
+      cursorline = 'CursorLine', -- cursor line
+      cursorlinenr = 'CursorLineNr', -- cursor line number
+      search = 'IncSearch', -- search matches (ctags|help)
+      title = 'FzfLuaNormal', -- preview border title (file/buffer)
+      -- Only used with 'winopts.preview.scrollbar = 'float'
+      scrollfloat_e = 'PmenuSbar', -- scrollbar "empty" section highlight
+      scrollfloat_f = 'PmenuThumb', -- scrollbar "full" section highlight
+      -- Only used with 'winopts.preview.scrollbar = 'border'
+      scrollborder_e = 'FloatBorder', -- scrollbar "empty" section highlight
+      scrollborder_f = 'FloatBorder', -- scrollbar "full" section highlight
+    },
+    preview = {
+      -- default = 'cat',
+      border = 'border',
+      -- hidden = 'hidden',
+      title = false,
+      winopts = {
+        number = false,
+        relativenumber = false,
+        cursorline = true,
+        cursorlineopt = 'both',
+        cursorcolumn = false,
+        signcolumn = 'no',
+        list = false,
+        foldenable = false,
+        foldmethod = 'manual',
+      },
+    },
+  },
 })
