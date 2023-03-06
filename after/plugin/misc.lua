caab3b6acaba98e864e3e3abec83f311249d831f
Dongsheng Cai <d@tux.im>
2023-03-06 16:42:48 +1100

    nvim: which-key

--- after/plugin/misc.lua
+++ after/plugin/misc.lua
@@ -1,3 +1,8 @@
+local whichkey_loaded, whichkey = pcall(require, 'which-key')
+if not whichkey_loaded then
+  return
+end
+
 require('which-key').setup({
   -- your configuration comes here
   -- or leave it empty to use the default settings
