16955218bd099de38fa07fdfc1037edbc1ab93af
dcai <d@tux.im>
2023-10-23 13:31:44 +1100

    lua: stop using some global functions

--- after/plugin/notes.lua
+++ after/plugin/notes.lua
@@ -1,20 +1,20 @@
 vim.g.notes_home = '~/Library/CloudStorage/Dropbox/Documents/txt'
 local fzflua_loaded, fzflua = pcall(require, 'fzf-lua')
 
-function note_today()
+local note_today = function()
   return os.date('%Y%m%d')
 end
 
-function note_now()
+local note_now = function()
   return os.date('%Y-%m-%d %H:%M')
 end
 
-function note_insert_text(text)
+local note_insert_text = function(text)
   local str = [[ exec "normal Go%s\<cr>\<esc>G" ]]
   vim.cmd(string.format(str, text))
 end
 
-function note_run_cmd(cmd)
+local note_run_cmd = function(cmd)
   local result = vim.fn.systemlist(cmd)[1]
   if vim.v.shell_error == 0 then
     return result
@@ -22,22 +22,22 @@ function note_run_cmd(cmd)
   return nil
 end
 
-function note_get_git_root()
+local note_get_git_root = function()
   return note_run_cmd('git rev-parse --show-toplevel | xargs basename')
 end
 
-function note_get_git_branch()
+local note_get_git_branch = function()
   return note_run_cmd('git symbolic-ref --short HEAD')
 end
 
-function note_create_or_edit(name)
+local note_create_or_edit = function(name)
   local filename = slugify(name, '-')
   local dir = vim.g.notes_home
   local path = vim.fn.expand(string.format('%s/%s.md', dir, filename))
-  vim.cmd(f('edit {path}'))
+  vim.cmd('edit ' .. path)
 end
 
-function create_note_for_project(with_branch)
+local create_note_for_project = function(with_branch)
   return function()
     local now = note_now()
     local git_root = slugify(note_get_git_root(), '-')
@@ -48,21 +48,21 @@ function create_note_for_project(with_branch)
 
     if git_root and git_branch then
       local dir = vim.g.notes_home
-      local project_dir = vim.fn.expand(f('{dir}/projects/{git_root}'))
+      local project_dir = vim.fn.expand(dir .. '/projects/' .. git_root)
       vim.fn.mkdir(project_dir, 'p')
-      local filename = f('{project_dir}/{git_branch}.md')
-      vim.cmd(f('edit {filename}'))
+      local filename = project_dir .. '/' .. git_branch .. 'md'
+      vim.cmd('edit ' .. filename)
       note_insert_text(string.format('###### %s', now))
     end
   end
 end
 
 vim.api.nvim_create_user_command('NoteToday', function()
-  local today = note_today()
+  -- local today = note_today()
   local now = note_now()
-  local dir = vim.g.notes_home
-  local filename = f('{dir}/Journal/{today}.md')
-  vim.cmd(f('edit {filename}'))
+  -- local dir = vim.g.notes_home
+  local filename = dir .. '/Journal/' .. today .. '.md'
+  vim.cmd('edit ' .. filename)
   note_insert_text(string.format('###### %s', now))
 end, {})
 
