80afa9362aac1541f7110f19eb850dbf06be45b4
dcai <d@tux.im>
2024-02-20 21:44:41 +1100

    codeium: disable on env var

--- after/plugin/cmp_userconfig.lua
+++ after/plugin/cmp_userconfig.lua
@@ -1,5 +1,12 @@
 local codeium_loaded, codeium = pcall(require, 'codeium')
 if codeium_loaded then
+  vim.cmd([[
+    let g:codeium_no_map_tab = v:true
+    imap <script><silent><nowait><expr> <C-f> codeium#Accept()
+    imap <C-j> <Cmd>call codeium#CycleCompletions(1)<CR>
+    imap <C-k> <Cmd>call codeium#CycleCompletions(-1)<CR>
+    imap <C-x> <Cmd>call codeium#Clear()<CR>
+  ]])
   codeium.setup({})
 end
 
