7fd9d67901c9a551cc831be6cb022256af077e2f
dcai <d@tux.im>
2024-02-24 22:58:48 +1100

    python and node providers

--- init.lua
+++ init.lua
@@ -3,9 +3,13 @@ require('global_utils')
 vim.g.loaded_perl_provider = 0
 vim.g.loaded_ruby_provider = 0
 vim.g.python3_host_prog = find_executable({
+  '~/.local/share/nvim/venv/bin/python3',
   '/opt/homebrew/bin/python3',
   '/usr/local/bin/python3',
   '/usr/bin/python3',
+})
+vim.g.node_host_prog = find_executable({
+  '~/.npm-packages/bin/neovim-node-host',
 })
 
 vim.opt.cmdheight = 0
