ea75fb258f49009adb4e8e2af65ecc528cb996a5
dcai <d@tux.im>
2024-02-29 14:43:46 +1100

    luasnip

--- after/plugin/luasnip_userconfig.lua
+++ after/plugin/luasnip_userconfig.lua
@@ -1,8 +1,17 @@
-local loaded, luasnip = pcall(require, 'luasnip')
+local loaded, ls = pcall(require, 'luasnip')
 if loaded then
-  local fromVscode = require('luasnip.loaders.from_vscode')
-  fromVscode.lazy_load({})
-  fromVscode.lazy_load({
-    paths = { './src/snippets' },
+  ls.filetype_extend('typescript', { 'javascript' })
+  -- load built in
+  require('luasnip.loaders.from_vscode').lazy_load()
+  -- load from custom folders
+  require('luasnip.loaders.from_vscode').lazy_load({
+    paths = {
+      '~/src/vssnips',
+    },
+  })
+  local fromSnipmate = require('luasnip.loaders.from_snipmate')
+  fromSnipmate.lazy_load()
+  fromSnipmate.lazy_load({
+    paths = { '~/src/test-snip' },
   })
 end
