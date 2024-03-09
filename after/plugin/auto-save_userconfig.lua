88203b46414ae9fed75405add8aa9b47a3ea4251
dcai <d@tux.im>
2024-03-10 00:15:51 +1100

    auto-save: fix

--- after/plugin/auto-save_userconfig.lua
+++ after/plugin/auto-save_userconfig.lua
@@ -1,12 +1,12 @@
-local loaded, autoSave = pcall(require, 'auto-save')
+local loaded, autosave = pcall(require, 'auto-save')
 if not loaded then
   return
 end
-autoSave.setup({
+autosave.setup({
   enabled = true, -- start auto-save when the plugin is loaded (i.e. when your package manager loads it)
   execution_message = {
     message = function() -- message to print on save
-      return ('AutoSave: saved at ' .. vim.fn.strftime('%H:%M:%S'))
+      return ('auto-save: saved at ' .. vim.fn.strftime('%H:%M:%S'))
     end,
     dim = 0.18, -- dim the color of `message`
     cleaning_interval = 1250, -- (milliseconds) automatically clean MsgArea after displaying `message`. See :h MsgArea
@@ -17,19 +17,13 @@ autoSave.setup({
   -- return false: if it's not ok to be saved
   condition = function(buf)
     local fn = vim.fn
-    local utils = require('auto-save.utils.data')
-
-    -- https://github.com/ThePrimeagen/harpoon/issues/434#issuecomment-1864904972
-    if vim.bo[buf].filetype == 'harpoon' then
-      return false
+    local modifiable = fn.getbufvar(buf, '&modifiable') == 1
+    local ft = fn.getbufvar(buf, '&filetype')
+    local blacklist = { 'sagafinder', 'harpoon' }
+    if modifiable and not vim.tbl_contains(blacklist, ft) then
+      return true
     end
-    if
-      fn.getbufvar(buf, '&modifiable') == 1
-      and utils.not_in(fn.getbufvar(buf, '&filetype'), {})
-    then
-      return true -- met condition(s), can save
-    end
-    return false -- can't save
+    return false
   end,
   write_all_buffers = false, -- write all buffers when the current one meets `condition`
   debounce_delay = 135, -- saves the file at most every `debounce_delay` milliseconds
