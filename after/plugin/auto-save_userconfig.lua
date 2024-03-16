78d9dcd7350982bd1d5c90e33830ec51ee6c918f
dcai <d@tux.im>
2024-03-16 23:59:41 +1100

    autosave: debounce_delay

--- after/plugin/auto-save_userconfig.lua
+++ after/plugin/auto-save_userconfig.lua
@@ -2,19 +2,19 @@ local loaded, autosave = pcall(require, 'auto-save')
 if not loaded then
   return
 end
+-- https://github.com/pocco81/auto-save.nvim
 autosave.setup({
-  enabled = true, -- start auto-save when the plugin is loaded (i.e. when your package manager loads it)
+  enabled = true,
+  -- saves the file at most every `debounce_delay` milliseconds
+  debounce_delay = 2000,
   execution_message = {
-    message = function() -- message to print on save
+    message = function()
       return ('auto-save: saved at ' .. vim.fn.strftime('%H:%M:%S'))
     end,
     dim = 0.18, -- dim the color of `message`
     cleaning_interval = 1250, -- (milliseconds) automatically clean MsgArea after displaying `message`. See :h MsgArea
   },
   trigger_events = { 'InsertLeave', 'TextChanged' }, -- vim events that trigger auto-save. See :h events
-  -- function that determines whether to save the current buffer or not
-  -- return true: if buffer is ok to be saved
-  -- return false: if it's not ok to be saved
   condition = function(buf)
     local fn = vim.fn
     local modifiable = fn.getbufvar(buf, '&modifiable') == 1
@@ -26,7 +26,6 @@ autosave.setup({
     return false
   end,
   write_all_buffers = false, -- write all buffers when the current one meets `condition`
-  debounce_delay = 135, -- saves the file at most every `debounce_delay` milliseconds
   callbacks = { -- functions to be executed at different intervals
     enabling = nil, -- ran when enabling auto-save
     disabling = nil, -- ran when disabling auto-save
