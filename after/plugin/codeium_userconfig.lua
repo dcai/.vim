c5b249e9d9f50a3c0962f82e46039351af5a964c
dcai <d@tux.im>
2024-03-17 02:41:45 +1100

    codeium: fixup

--- after/plugin/codeium_userconfig.lua
+++ after/plugin/codeium_userconfig.lua
@@ -1,5 +1,6 @@
 local loaded, codeium = pcall(require, 'codeium')
 if loaded then
+  -- for Exafunction/codeium.vim
   vim.cmd([[
     let g:codeium_no_map_tab = v:true
     imap <script><silent><nowait><expr> <C-f> codeium#Accept()
@@ -7,5 +8,6 @@ if loaded then
     imap <C-k> <Cmd>call codeium#CycleCompletions(-1)<CR>
     imap <C-x> <Cmd>call codeium#Clear()<CR>
   ]])
+  -- for Exafunction/codeium.nvim
   codeium.setup({})
 end
