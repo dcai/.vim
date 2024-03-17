bc63dc1d65cac035f6614ab485d1494692c3ef38
Dong Cai <d@tux.im>
2024-03-17 22:15:31 +1100

    marlin

--- after/plugin/marlin_userconfig.lua
+++ after/plugin/marlin_userconfig.lua
@@ -4,24 +4,16 @@ if not loaded then
 end
 
 marlin.setup({
-  patterns = { '.git', '.svn' },
+  patterns = { '.git', 'package.json' },
   datafile = vim.fn.stdpath('data') .. '/marlin.json',
+  open_callback = require('marlin.callbacks').change_buffer,
+  sorter = require('marlin.sorters').by_name,
   suppress = {
     missing_root = false,
   },
 })
 
 local keymap = vim.keymap.set
-keymap('n', '<Leader>fa', function()
-  marlin.add()
-end, { desc = 'add file' })
-keymap('n', '<Leader>fd', function()
-  marlin.remove()
-end, { desc = 'remove file' })
--- keymap('n', '<Leader>fx', function()
---   marlin.remove_all()
--- end, { desc = 'remove all for current project' })
-
 for index = 1, 4 do
   keymap('n', '<Leader>' .. index, function()
     marlin.open(index)
