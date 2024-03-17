45e2d333c0fb30be623716d8676e4ecf90e9ce6a
dcai <d@tux.im>
2024-03-17 20:08:55 +1100

    whichkey: ui

--- after/plugin/whichkey_userconfig.lua
+++ after/plugin/whichkey_userconfig.lua
@@ -51,17 +51,17 @@ which_key.setup({
   window = {
     border = 'rounded', -- none, single, double, shadow
     position = 'bottom', -- bottom, top
-    margin = { 1, 0, 1, 0 }, -- extra window margin [top, right, bottom, left]
-    padding = { 2, 2, 2, 2 }, -- extra window padding [top, right, bottom, left]
+    margin = { 0, 0, 0, 0 }, -- extra window margin [top, right, bottom, left]
+    padding = { 1, 1, 1, 1 }, -- extra window padding [top, right, bottom, left]
     winblend = 0,
   },
   layout = {
-    height = { min = 5, max = 25 }, -- min and max height of the columns
+    height = { min = 3, max = 25 }, -- min and max height of the columns
     width = { min = 20, max = 50 }, -- min and max width of the columns
     spacing = 3, -- spacing between columns
-    align = 'left', -- align columns left, center or right
+    align = 'center', -- align columns left, center or right
   },
-  ignore_missing = true, -- enable this to hide mappings for which you didn't specify a label
+  ignore_missing = false, -- enable this to hide mappings for which you didn't specify a label
   hidden = { '<silent>', '<cmd>', '<Cmd>', '<CR>', 'call', 'lua', '^:', '^ ' }, -- hide mapping boilerplate
   show_help = true, -- show help message on the command line when the popup is visible
   triggers = 'auto', -- automatically setup triggers
@@ -76,7 +76,7 @@ which_key.setup({
 })
 
 ---generate opts for the plugin based on mode
----@param mode any
+---@param mode string
 ---@return table
 local function make_mapping_opts(mode)
   return {
@@ -166,7 +166,7 @@ end
 local fzf_keymap = {
   name = 'fzf',
   ['b'] = { fzf.buffers, 'buffers' },
-  ['c'] = { fzf.colorschemes, 'vim colorschemes' },
+  ['c'] = { fzf.colorschemes, 'colorschemes' },
   ['r'] = { fzf.oldfiles, 'recent files' },
   ['m'] = { marlin_marks, 'marlin files' },
 }
