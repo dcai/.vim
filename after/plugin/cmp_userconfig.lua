4aacf368bf3b0b51d5d4c21e2d0c604b0f2c4c4e
dcai <d@tux.im>
2024-03-25 00:58:48 +1100

    codeium

--- after/plugin/cmp_userconfig.lua
+++ after/plugin/cmp_userconfig.lua
@@ -4,6 +4,12 @@ if not cmp_loaded then
   return
 end
 
+local codeium_loaded, codeium = pcall(require, 'codeium')
+if codeium_loaded then
+  -- for Exafunction/codeium.nvim
+  codeium.setup({})
+end
+
 local function check_back_space()
   local col = vim.fn.col('.') - 1
   if col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') then
