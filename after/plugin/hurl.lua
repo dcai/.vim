483ac935c66934344fd2bc30a2eb713b35a513b5
dcai <heydcai@gmail.com>
2024-05-04 00:02:12 +1000

    hurl: use popup

--- after/plugin/hurl.lua
+++ after/plugin/hurl.lua
@@ -14,10 +14,31 @@ vim.api.nvim_create_user_command('HurlRun', function()
     return
   end
   local filepath = vim.fn.expand('%:p')
-  local testrunner = string.format(
-    'cd %s; hurl --color --insecure %s',
-    project_root,
-    filepath
-  )
-  vim.cmd(string.format('VimuxRunCommand("%s")', testrunner))
+  local popup = G.new_popup({ title = 'hurl', number = false })
+  local PJob = require('plenary.job')
+  popup.open()
+  local channel = vim.api.nvim_open_term(popup.buffer, {})
+  PJob:new({
+    command = 'hurl',
+    args = {
+      '--color',
+      '--insecure',
+      filepath,
+      '--variables-file',
+      project_root .. '/vars.txt',
+    },
+    cwd = project_root,
+    skip_validation = true,
+    on_exit = vim.schedule_wrap(function(job, status)
+      if status ~= 0 then
+        pcall(
+          vim.api.nvim_chan_send,
+          channel,
+          table.concat(job:stderr_result(), G.nl)
+        )
+      else
+        pcall(vim.api.nvim_chan_send, channel, table.concat(job:result(), G.nl))
+      end
+    end),
+  }):start()
 end, {})
