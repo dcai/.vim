d557908dc6327f54e3750fcbd4303782cc18ad7e
Dongsheng Cai <d@tux.im>
2024-05-06 10:23:26 +1000

    treesj: setup

--- after/plugin/whichkey_userconfig.lua
+++ after/plugin/whichkey_userconfig.lua
@@ -10,34 +10,34 @@ local vimrc_to_edit = '~/.config/nvim/after/plugin/whichkey_userconfig.lua'
 local zed = [[/Applications/Zed.app/Contents/MacOS/cli]]
 
 local tsj_loaded, tsj = pcall(require, 'treesj')
-if not loaded then
+if tsj_loaded then
+  tsj.setup({
+    ---@type boolean Use default keymaps (<space>m - toggle, <space>j - join, <space>s - split)
+    use_default_keymaps = false,
+    ---@type boolean Node with syntax error will not be formatted
+    check_syntax_error = true,
+    ---If line after join will be longer than max value,
+    ---@type number If line after join will be longer than max value, node will not be formatted
+    max_join_length = 120,
+    ---Cursor behavior:
+    ---hold - cursor follows the node/place on which it was called
+    ---start - cursor jumps to the first symbol of the node being formatted
+    ---end - cursor jumps to the last symbol of the node being formatted
+    ---@type 'hold'|'start'|'end'
+    cursor_behavior = 'hold',
+    ---@type boolean Notify about possible problems or not
+    notify = true,
+    ---@type boolean Use `dot` for repeat action
+    dot_repeat = true,
+    ---@type nil|function Callback for treesj error handler. func (err_text, level, ...other_text)
+    on_error = nil,
+    ---@type table Presets for languages
+    -- langs = {}, -- See the default presets in lua/treesj/langs
+  })
+else
   print('treesj not installed')
   return
 end
-
-tsj.setup({
-  ---@type boolean Use default keymaps (<space>m - toggle, <space>j - join, <space>s - split)
-  use_default_keymaps = false,
-  ---@type boolean Node with syntax error will not be formatted
-  check_syntax_error = true,
-  ---If line after join will be longer than max value,
-  ---@type number If line after join will be longer than max value, node will not be formatted
-  max_join_length = 120,
-  ---Cursor behavior:
-  ---hold - cursor follows the node/place on which it was called
-  ---start - cursor jumps to the first symbol of the node being formatted
-  ---end - cursor jumps to the last symbol of the node being formatted
-  ---@type 'hold'|'start'|'end'
-  cursor_behavior = 'hold',
-  ---@type boolean Notify about possible problems or not
-  notify = true,
-  ---@type boolean Use `dot` for repeat action
-  dot_repeat = true,
-  ---@type nil|function Callback for treesj error handler. func (err_text, level, ...other_text)
-  on_error = nil,
-  ---@type table Presets for languages
-  -- langs = {}, -- See the default presets in lua/treesj/langs
-})
 
 local function shell_cmd(command)
   return function(opts, desc)
