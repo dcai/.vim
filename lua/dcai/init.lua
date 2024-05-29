18d7d6cd607132b86acbc07640c78133aa084972
dcai <d@tux.im>
2024-05-29 13:55:28 +1000

    osc52: use osc52 when in ssh

--- lua/dcai/init.lua
+++ lua/dcai/init.lua
@@ -31,23 +31,26 @@ codestats.setup()
 
 -- Copy/Paste when using ssh on a remote server
 -- Only works on Neovim >= 0.10.0
-if vim.clipboard and vim.clipboard.osc52 then
+if
+  vim.env.SSH_CONNECTION
+  and vim.env.SSH_TTY
+  and vim.clipboard
+  and vim.clipboard.osc52
+then
   vim.api.nvim_create_autocmd('VimEnter', {
     group = vim.api.nvim_create_augroup('ssh_clipboard', { clear = true }),
     callback = function()
-      if vim.env.SSH_CONNECTION and vim.clipboard.osc52 then
-        vim.g.clipboard = {
-          name = 'OSC 52',
-          copy = {
-            ['+'] = require('vim.clipboard.osc52').copy,
-            ['*'] = require('vim.clipboard.osc52').copy,
-          },
-          paste = {
-            ['+'] = require('vim.clipboard.osc52').paste,
-            ['*'] = require('vim.clipboard.osc52').paste,
-          },
-        }
-      end
+      vim.g.clipboard = {
+        name = 'OSC 52',
+        copy = {
+          ['+'] = require('vim.clipboard.osc52').copy,
+          ['*'] = require('vim.clipboard.osc52').copy,
+        },
+        paste = {
+          ['+'] = require('vim.clipboard.osc52').paste,
+          ['*'] = require('vim.clipboard.osc52').paste,
+        },
+      }
     end,
   })
 end
