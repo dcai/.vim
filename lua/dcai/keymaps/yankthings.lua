caed48fa47ec6d34c40e2e54d5e7eed09230a577
dcai <heydcai@gmail.com>
2024-12-04 10:21:41 +1100

    yank: split lines

--- lua/dcai/keymaps/yankthings.lua
+++ lua/dcai/keymaps/yankthings.lua
@@ -4,7 +4,8 @@ local function put_content(contents)
   vim.fn.setreg('*', contents)
   vim.cmd('enew')
   vim.bo.buftype = 'nofile'
-  vim.bo.bufhidden = 'hide'
+  -- vim.bo.bufhidden = 'hide'
+  vim.bo.bufhidden = 'wipe'
   vim.bo.swapfile = false
   local lines = { contents }
   if type(contents) == 'table' then
@@ -15,6 +16,9 @@ end
 
 local yank_keymap = {
   { '<leader>y', group = 'yank things' },
+  -- utils.vim_cmd('<leader>yp', 'let @*=expand("%:p")', 'yank file full path'),
+  -- utils.vim_cmd('<leader>yf', 'let @*=expand("%")', 'yank file name'),
+  -- utils.vim_cmd('<leader>ym', 'let @*=execute("messages")', 'yank messages'),
   {
     '<leader>yl',
     function()
@@ -43,17 +47,17 @@ local yank_keymap = {
     '<leader>ym',
     function()
       local messages = vim.fn.execute('messages')
-      put_content(messages)
+      put_content(vim.split(messages, '\n', { trimempty = true, plain = true }))
     end,
     desc = 'yank full file path',
   },
-  -- utils.vim_cmd(
-  --   '<leader>yp',
-  --   'let @*=expand("%:p")',
-  --   'yank file full path',
-  --   'file path yanked'
-  -- ),
-  -- utils.vim_cmd('<leader>yf', 'let @*=expand("%")', 'yank file name'),
-  -- utils.vim_cmd('<leader>ym', 'let @*=execute("messages")', 'yank messages'),
+  {
+    '<leader>yk',
+    function()
+      local messages = vim.fn.execute('map')
+      put_content(vim.split(messages, '\n', { trimempty = true, plain = true }))
+    end,
+    desc = 'yank map output',
+  },
 }
 return yank_keymap
