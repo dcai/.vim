6be66686e89bc128bd52db68f7abaeca530cad15
dcai <heydcai@gmail.com>
2024-12-04 01:48:20 +1100

    yank: paste full path to scratch

--- lua/dcai/keymaps/yankthings.lua
+++ lua/dcai/keymaps/yankthings.lua
@@ -1,4 +1,18 @@
 local utils = require('dcai.keymaps.utils')
+
+local function put_content(contents)
+  vim.fn.setreg('*', contents)
+  vim.cmd('enew')
+  vim.bo.buftype = 'nofile'
+  vim.bo.bufhidden = 'hide'
+  vim.bo.swapfile = false
+  local lines = { contents }
+  if type(contents) == 'table' then
+    lines = contents
+  end
+  vim.api.nvim_put(lines, 'c', true, true)
+end
+
 local yank_keymap = {
   { '<leader>y', group = 'yank things' },
   {
@@ -9,13 +23,37 @@ local yank_keymap = {
     end,
     desc = 'yank loaded package names',
   },
-  utils.vim_cmd(
+  {
     '<leader>yp',
-    'let @*=expand("%:p")',
-    'yank file full path',
-    'file path yanked'
-  ),
-  utils.vim_cmd('<leader>yf', 'let @*=expand("%")', 'yank file name'),
-  utils.vim_cmd('<leader>ym', 'let @*=execute("messages")', 'yank messages'),
+    function()
+      local filepath = vim.fn.expand('%:~')
+      put_content(filepath)
+    end,
+    desc = 'yank full file path',
+  },
+  {
+    '<leader>yf',
+    function()
+      local filename = vim.fn.expand('%')
+      put_content(filename)
+    end,
+    desc = 'yank full file path',
+  },
+  {
+    '<leader>ym',
+    function()
+      local messages = vim.fn.execute('messages')
+      put_content(messages)
+    end,
+    desc = 'yank full file path',
+  },
+  -- utils.vim_cmd(
+  --   '<leader>yp',
+  --   'let @*=expand("%:p")',
+  --   'yank file full path',
+  --   'file path yanked'
+  -- ),
+  -- utils.vim_cmd('<leader>yf', 'let @*=expand("%")', 'yank file name'),
+  -- utils.vim_cmd('<leader>ym', 'let @*=execute("messages")', 'yank messages'),
 }
 return yank_keymap
