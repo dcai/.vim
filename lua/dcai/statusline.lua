ec9da5fcc0eefa6d6c8266bae4ca80f688f6bae0
dcai <d@tux.im>
2025-01-10 15:25:11 +1100

    statusline: project name should be from git root

--- lua/dcai/statusline.lua
+++ lua/dcai/statusline.lua
@@ -41,7 +41,7 @@ local function current_buffer_name()
   local fullpath = vim.fn.expand('%:p')
   local filemodified = ' %m'
   local readonly = '%r'
-  local project_root = G.smart_root()
+  local project_root = G.git_root()
   local project_name = project_root:match('([^/]+)$')
   local project_root_trimed = G.replace(fullpath, G.smart_root(), '@')
   return (project_name and color_accent(project_name) or '')
