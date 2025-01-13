5e06d3ba05f7c0a3c285b664c654318711f0cd1b
dcai <d@tux.im>
2025-01-13 20:34:13 +1100

    mini: cleanup

--- after/plugin/mini_userconfig.lua
+++ after/plugin/mini_userconfig.lua
@@ -4,6 +4,48 @@ if not loaded then
   return
 end
 
+mini_surround.setup({
+  -- Add custom surroundings to be used on top of builtin ones. For more
+  -- information with examples, see `:h MiniSurround.config`.
+  custom_surroundings = nil,
+
+  -- Duration (in ms) of highlight when calling `MiniSurround.highlight()`
+  highlight_duration = 500,
+
+  -- Module mappings. Use `''` (empty string) to disable one.
+  mappings = {
+    replace = 'cs', -- Replace surrounding
+    delete = 'ds', -- Delete surrounding
+    --- don't use ys as it slows down the yank action
+    add = 'sa', -- Add surrounding in Normal and Visual modes
+
+    find = '', -- Find surrounding (to the right)
+    find_left = '', -- Find surrounding (to the left)
+    highlight = '', -- Highlight surrounding
+    update_n_lines = '', -- Update `n_lines`
+
+    suffix_last = '', -- Suffix to search with "prev" method
+    suffix_next = '', -- Suffix to search with "next" method
+  },
+
+  -- Number of lines within which surrounding is searched
+  n_lines = 20,
+
+  -- Whether to respect selection type:
+  -- - Place surroundings on separate lines in linewise mode.
+  -- - Place surroundings on each line in blockwise mode.
+  respect_selection_type = false,
+
+  -- How to search for surrounding (first inside current line, then inside
+  -- neighborhood). One of 'cover', 'cover_or_next', 'cover_or_prev',
+  -- 'cover_or_nearest', 'next', 'prev', 'nearest'. For more details,
+  -- see `:h MiniSurround.config`.
+  search_method = 'cover',
+
+  -- Whether to disable showing non-error feedback
+  silent = false,
+})
+
 -- local animate = require('mini.animate')
 -- animate.setup({
 --   -- Cursor path
@@ -60,53 +102,11 @@ end
 --   },
 -- })
 
-mini_surround.setup({
-  -- Add custom surroundings to be used on top of builtin ones. For more
-  -- information with examples, see `:h MiniSurround.config`.
-  custom_surroundings = nil,
-
-  -- Duration (in ms) of highlight when calling `MiniSurround.highlight()`
-  highlight_duration = 500,
-
-  -- Module mappings. Use `''` (empty string) to disable one.
-  mappings = {
-    replace = 'cs', -- Replace surrounding
-    delete = 'ds', -- Delete surrounding
-    --- don't use ys as it slows down the yank action
-    add = 'sa', -- Add surrounding in Normal and Visual modes
-
-    find = '', -- Find surrounding (to the right)
-    find_left = '', -- Find surrounding (to the left)
-    highlight = '', -- Highlight surrounding
-    update_n_lines = '', -- Update `n_lines`
-
-    suffix_last = '', -- Suffix to search with "prev" method
-    suffix_next = '', -- Suffix to search with "next" method
-  },
-
-  -- Number of lines within which surrounding is searched
-  n_lines = 20,
-
-  -- Whether to respect selection type:
-  -- - Place surroundings on separate lines in linewise mode.
-  -- - Place surroundings on each line in blockwise mode.
-  respect_selection_type = false,
-
-  -- How to search for surrounding (first inside current line, then inside
-  -- neighborhood). One of 'cover', 'cover_or_next', 'cover_or_prev',
-  -- 'cover_or_nearest', 'next', 'prev', 'nearest'. For more details,
-  -- see `:h MiniSurround.config`.
-  search_method = 'cover',
-
-  -- Whether to disable showing non-error feedback
-  silent = false,
-})
-
 local context_commentstring_loaded, context_commentstring =
   pcall('require', 'ts_context_commentstring')
 if context_commentstring_loaded then
   context_commentstring.setup({
-    enable_autocmd = false,
+    enable_autocmd = true,
   })
 end
 require('mini.comment').setup({
