480fe4bdb98a262c0ed0a9caff032f06a2bd63d2
dcai <d@tux.im>
2025-03-08 16:28:12 +1100

    feat: Display hurl output in a buffer
    This commit changes the way hurl output is displayed. Instead of using a popup window, the output is now displayed in a buffer. This allows for better integration with the editor and makes it easier to view and interact with the output.

--- after/plugin/hurl.lua
+++ after/plugin/hurl.lua
@@ -7,30 +7,42 @@ vim.api.nvim_create_user_command('HurlRun', function()
     return
   end
   local filepath = vim.fn.expand('%:p')
-  local popup = vim.g.new_popup({ title = 'hurl', number = false })
+  -- local popup = vim.g.new_popup({ title = 'hurl', number = false })
+  -- popup.open()
   local PJob = require('plenary.job')
-  popup.open()
-  local channel = vim.api.nvim_open_term(popup.buffer, {})
+
+  local bufnr = vim.api.nvim_create_buf(false, true)
+  vim.api.nvim_set_current_buf(bufnr)
+  -- local channel = vim.api.nvim_open_term(popup.buffer, {})
+  -- local channel = vim.api.nvim_open_term(bufnr, {})
+  -- local hr = vim.g.nl .. '============' .. vim.g.nl
+  vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, {
+    'loading...',
+  })
+
+  local args = {
+    '--color',
+    '--insecure',
+    filepath,
+  }
+
+  if dir and vim.g.file_exists(dir .. '/vars.txt') then
+    table.insert(args, '--variables-file')
+    table.insert(args, dir .. '/vars.txt')
+  end
+
   PJob:new({
     command = 'hurl',
-    args = {
-      '--color',
-      '--insecure',
-      filepath,
-      '--variables-file',
-      dir .. '/vars.txt',
-    },
+    args = args,
     cwd = dir,
     skip_validation = true,
     on_exit = vim.schedule_wrap(function(job, status)
+      vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, {})
+      vim.api.nvim_set_option_value('filetype', 'json', { buf = bufnr })
       if status ~= 0 then
-        pcall(
-          vim.api.nvim_chan_send,
-          channel,
-          table.concat(job:stderr_result(), vim.g.nl)
-        )
+        vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, job:stderr_result())
       else
-        pcall(vim.api.nvim_chan_send, channel, table.concat(job:result(), vim.g.nl))
+        vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, job:result())
       end
     end),
   }):start()
