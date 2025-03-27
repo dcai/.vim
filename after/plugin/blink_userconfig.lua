c6d8e117c78d1924ecec8fffb0ae0dead7519570
dcai <d@tux.im>
2025-03-27 21:32:29 +1100

    chore(blink_userconfig.lua): comment out documentation scrolling keybindings

--- after/plugin/blink_userconfig.lua
+++ after/plugin/blink_userconfig.lua
@@ -40,8 +40,8 @@ require('blink.cmp').setup({
     ['<C-p>'] = { 'select_prev', 'fallback_to_mappings' },
     ['<C-n>'] = { 'select_next', 'fallback_to_mappings' },
 
-    ['<C-b>'] = { 'scroll_documentation_up', 'fallback' },
-    ['<C-f>'] = { 'scroll_documentation_down', 'fallback' },
+    -- ['<C-b>'] = { 'scroll_documentation_up', 'fallback' },
+    -- ['<C-f>'] = { 'scroll_documentation_down', 'fallback' },
 
     ['<C-k>'] = { 'show_signature', 'hide_signature', 'fallback' },
   },
