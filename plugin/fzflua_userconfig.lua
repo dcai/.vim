615701a44267fd1453d0dfde16bbb9db42f589cf
dcai <d@tux.im>
2025-04-11 22:16:38 +1000

    refactor: Refactor fzf-lua user configuration
    This commit refactors the fzf-lua user configuration to improve usability and maintainability.
    
    - Re-enable default border style.
    - Remove default keymaps.
    - Update default file actions.
    - Update default file commands.
    - Update default grep commands.
    - Update default colorscheme actions.

--- plugin/fzflua_userconfig.lua
+++ plugin/fzflua_userconfig.lua
@@ -30,9 +30,9 @@ fzflua.setup({
     row = 1, -- window row position (0=top, 1=bottom)
     col = 1, -- window col position (0=left, 1=right)
     height = 0.5,
-    fullscreen = false, -- start fullscreen?
-    border = false,
-    -- border = { '╭', '─', '╮', '│', '╯', '─', '╰', '│' },
+    fullscreen = false,
+    -- border = false,
+    border = { '╭', '─', '╮', '│', '╯', '─', '╰', '│' },
     preview = {
       default = 'bat',
       -- border = 'noborder',
@@ -60,25 +60,7 @@ fzflua.setup({
     gutter = 'LineNr',
   },
   keymap = {
-    -- These override the default tables completely
-    -- no need to set to `false` to disable a bind
-    -- delete or modify is sufficient
-    builtin = {
-      -- neovim `:tmap` mappings for the fzf win
-      ['<F1>'] = 'toggle-help',
-      ['<F2>'] = 'toggle-fullscreen',
-      -- Only valid with the 'builtin' previewer
-      ['<F3>'] = 'toggle-preview-wrap',
-      ['<F4>'] = 'toggle-preview',
-      -- Rotate preview clockwise/counter-clockwise
-      ['<F5>'] = 'toggle-preview-ccw',
-      ['<F6>'] = 'toggle-preview-cw',
-      ['<S-down>'] = 'preview-page-down',
-      ['<S-up>'] = 'preview-page-up',
-      ['<S-left>'] = 'preview-page-reset',
-    },
     fzf = {
-      -- fzf '--bind=' options
       ['ctrl-z'] = 'abort',
       ['ctrl-u'] = 'unix-line-discard',
       ['ctrl-a'] = 'beginning-of-line',
@@ -106,15 +88,9 @@ fzflua.setup({
   --   ['--bind'] = 'esc:abort',
   --   ['--history'] = history_dir .. '-default.txt',
   -- },
-  defaults = {
-    -- formatter = 'path.filename_first',
-  },
   files = {
     actions = {
-      ['default'] = actions.file_edit,
-      -- inherits from 'actions.files', here we can override
-      -- or set bind to 'false' to disable a default action
-      -- action to toggle `--no-ignore`, requires fd or rg installed
+      ['enter'] = actions.file_edit,
       ['ctrl-i'] = { actions.toggle_ignore },
       ['ctrl-h'] = { actions.toggle_hidden },
       ['ctrl-g'] = false,
@@ -123,60 +99,40 @@ fzflua.setup({
         vim.fn.setreg([[*]], path_helper.entry_to_file(selected[1]).path)
       end,
     },
-    -- previewer      = "bat",          -- uncomment to override previewer
-    -- (name from 'previewers' table)
-    -- set to 'false' to disable
+    -- previewer = "bat",
     prompt = 'Files❯ ',
     multiprocess = true, -- run command in a separate process
     git_icons = git_icons, -- show git icons?
     file_icons = use_icons, -- show file icons?
     color_icons = color_icons, -- colorize file|git icons
-    -- path_shorten   = 1,              -- 'true' or number, shorten path?
-    -- Uncomment for custom vscode-like formatter where the filename is first:
-    -- e.g. "fzf-lua/previewer/fzf.lua" => "fzf.lua previewer/fzf-lua"
-    -- formatter      = "path.filename_first",
-    -- executed command priority is 'cmd' (if exists)
-    -- otherwise auto-detect prioritizes `fd`:`rg`:`find`
-    -- default options are controlled by 'fd|rg|find|_opts'
-    -- NOTE: 'find -printf' requires GNU find
-    -- cmd = "find . -type f -printf '%P\n'",
-    cmd = 'rg --files --sortr=modified --hidden',
+    cmd = 'rg --files --sortr=modified',
     find_opts = [[-type f -not -path '*/\.git/*' -printf '%P\n']],
     rg_opts = [[--color=never --files --hidden --follow --glob "!.git"]],
     fd_opts = [[--color=never --type f --hidden --follow --exclude .git]],
-    -- fzf_opts = {
-    --   ['--history'] = history_dir .. '-files.txt',
-    -- },
-    -- by default, cwd appears in the header only if {opts} contain a cwd
-    -- parameter to a different folder than the current working directory
-    -- uncomment if you wish to force display of the cwd as part of the
-    -- query prompt string (fzf.vim style), header line or both
-    -- cwd_header = true,
+    dir_opts = [[/s/b/a:-d]],
+    cwd_header = false,
     cwd_prompt = true,
     cwd_prompt_shorten_len = 32, -- shorten prompt beyond this length
     cwd_prompt_shorten_val = 1, -- shortened path parts length
     toggle_ignore_flag = '--no-ignore', -- flag toggled in `actions.toggle_ignore`
+    toggle_hidden_flag = '--hidden', -- flag toggled in `actions.toggle_hidden`
+    hidden = true,
+    follow = true,
+    no_ignore = false,
   },
   grep = {
     prompt = 'grep❯ ',
-    multiprocess = true, -- run command in a separate process
-    git_icons = git_icons, -- show git icons?
-    file_icons = use_icons, -- show file icons?
-    color_icons = color_icons, -- colorize file|git icons
-    -- executed command priority is 'cmd' (if exists)
-    -- otherwise auto-detect prioritizes `rg` over `grep`
-    -- default options are controlled by 'rg|grep_opts'
+    multiprocess = true,
+    git_icons = git_icons,
+    file_icons = use_icons,
+    color_icons = color_icons,
     cmd = nil,
     grep_opts = '--binary-files=without-match --line-number --recursive --color=auto --perl-regexp -e',
     rg_opts = '--column --line-number --no-heading --color=always --smart-case '
       .. '--max-columns=4096 -e',
-    -- set to 'true' to always parse globs in both 'grep' and 'live_grep'
-    -- search strings will be split using the 'glob_separator' and translated
-    -- to '--iglob=' arguments, requires 'rg'
-    -- can still be used when 'false' by calling 'live_grep_glob' directly
-    rg_glob = false, -- default to glob parsing?
-    glob_flag = '--iglob', -- for case sensitive globs use '--glob'
-    glob_separator = '%s%-%-', -- query separator pattern (lua): ' --'
+    rg_glob = false,
+    glob_flag = '--iglob',
+    glob_separator = '%s%-%-',
     -- advanced usage: for custom argument parsing define
     -- 'rg_glob_fn' to return a pair:
     --   first returned argument is the new search query
@@ -186,30 +142,18 @@ fzflua.setup({
     --   return new_query, flags
     -- end,
     actions = {
-      -- actions inherit from 'actions.files' and merge
-      -- this action toggles between 'grep' and 'live_grep'
       ['ctrl-r'] = { actions.grep_lgrep },
-      -- uncomment to enable '.gitignore' toggle for grep
       ['ctrl-i'] = { actions.toggle_ignore },
       ['ctrl-g'] = false,
     },
     no_header = false, -- hide grep|cwd header?
     no_header_i = false, -- hide interactive header?
-    -- fzf_opts = {
-    --   ['--history'] = history_dir .. '-grep.txt',
-    -- },
   },
   colorschemes = {
     prompt = 'Colorschemes❯ ',
     live_preview = true,
     winopts = { height = 1, width = 0.20 },
-    actions = { ['default'] = actions.colorscheme },
-    -- uncomment to ignore colorschemes names (lua patterns)
-    -- ignore_patterns   = { "^delek$", "^blue$" },
-    -- uncomment to execute a callback on preview|close
-    -- e.g. a call to reset statusline highlights
-    -- cb_preview        = function() ... end,
-    -- cb_exit           = function() ... end,
+    actions = { ['enter'] = actions.colorscheme },
   },
   awesome_colorschemes = {
     prompt = 'Colorschemes❯ ',
@@ -224,7 +168,7 @@ fzflua.setup({
       ['--tiebreak'] = 'index',
     },
     actions = {
-      ['default'] = actions.colorscheme,
+      ['enter'] = actions.colorscheme,
       ['ctrl-/'] = { fn = actions.cs_update, reload = true },
       ['ctrl-b'] = { fn = actions.toggle_bg, exec_silent = true },
       ['ctrl-g'] = false,
@@ -284,7 +228,7 @@ fzflua.setup({
       -- git-delta is automatically detected as pager, uncomment to disable
       -- preview_pager = false,
       actions = {
-        ['default'] = function(selected, _)
+        ['enter'] = function(selected, _)
           local line = selected[1]
           local commit_hash = line:match('[^ ]+')
           vim.cmd('Git show --format=fuller ' .. commit_hash)
