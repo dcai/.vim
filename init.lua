e722ccd1d96c71979ad89d6ff36b45679de57b44
dcai <d@tux.im>
2025-04-29 16:59:19 +1000

    chore: update lazy.nvim path in init.lua

--- init.lua
+++ init.lua
@@ -3,14 +3,14 @@ vim.cmd('syntax off')
 -- vim.loader.enable()
 require('dcai')
 
--- local lazypath = G.data_dir .. '/lazy/lazy.nvim'
+-- local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 -- if not (vim.uv or vim.loop).fs_stat(lazypath) then
 --   vim.fn.system({
 --     'git',
 --     'clone',
 --     '--filter=blob:none',
 --     'https://github.com/folke/lazy.nvim.git',
---     '--branch=stable', -- latest stable release
+--     '--branch=stable',
 --     lazypath,
 --   })
 -- end
