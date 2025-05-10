6706e3efe2440ae61b03adcba1814a683e1e199b
dcai <d@tux.im>
2025-05-10 19:45:36 +1000

    feat(codecompanion): Refactor and add diff strategy
    This commit refactors the CodeCompanion plugin to improve its structure and add a new "diff" strategy.
    
    The following changes were made:
    
    - Moved configuration options for inline and chat strategies into their respective sections.
    - Added a new "diff" strategy with options for layout, provider, and diff options.
    - Updated default log level to INFO.

--- lua/dcai/llm/codecompanion.lua
+++ lua/dcai/llm/codecompanion.lua
@@ -87,7 +87,6 @@ M.setup = function()
     strategies = {
       inline = {
         adapter = provider,
-        layout = 'buffer', -- vertical|horizontal|buffer
         keymaps = {
           accept_change = {
             modes = { n = 'ga' },
@@ -114,14 +113,6 @@ M.setup = function()
           ---The header name for your messages
           ---@type string
           user = 'Me',
-          intro_message = 'Welcome to CodeCompanion ✨! Press ? for options',
-          show_header_separator = true,
-          separator = '─',
-          auto_scroll = false,
-          show_references = true, -- Show references (from slash commands and variables) in the chat buffer?
-          show_settings = true, -- Show LLM settings at the top of the chat buffer?
-          show_token_count = true, -- Show the token count for each response?
-          start_in_insert_mode = false, -- Open the chat buffer in insert mode?
         },
         slash_commands = {
           ['help'] = {
@@ -236,9 +227,80 @@ M.setup = function()
           show_default_prompt_library = true,
         },
       },
+      diff = {
+        enabled = true,
+        close_chat_at = 240, -- Close an open chat buffer if the total columns of your display are less than...
+        layout = 'vertical', -- vertical|horizontal split for default provider
+        opts = {
+          'internal',
+          'filler',
+          'closeoff',
+          'algorithm:patience',
+          'followwrap',
+          'linematch:120',
+        },
+        provider = 'default', -- default|mini_diff
+      },
+      inline = {
+        layout = 'vertical', -- vertical|horizontal|buffer
+      },
+      chat = {
+        intro_message = 'Welcome to CodeCompanion ✨! Press ? for options',
+        auto_scroll = false,
+        show_header_separator = false, -- Show header separators in the chat buffer? Set this to false if you're using an external markdown formatting plugin
+        separator = '─', -- The separator between the different messages in the chat buffer
+        show_references = true, -- Show references (from slash commands and variables) in the chat buffer?
+        show_settings = true, -- Show LLM settings at the top of the chat buffer?
+        show_token_count = true, -- Show the token count for each response?
+        start_in_insert_mode = false, -- Open the chat buffer in insert mode?
+        -- Change the default icons
+        icons = {
+          pinned_buffer = ' ',
+          watched_buffer = '👀 ',
+        },
+
+        -- Alter the sizing of the debug window
+        debug_window = {
+          ---@return number|fun(): number
+          width = vim.o.columns - 5,
+          ---@return number|fun(): number
+          height = vim.o.lines - 2,
+        },
+
+        -- Options to customize the UI of the chat buffer
+        window = {
+          layout = 'vertical', -- float|vertical|horizontal|buffer
+          position = nil, -- left|right|top|bottom (nil will default depending on vim.opt.plitright|vim.opt.splitbelow)
+          border = 'single',
+          height = 0.8,
+          width = 0.45,
+          relative = 'editor',
+          full_height = true, -- when set to false, vsplit will be used to open the chat buffer vs. botright/topleft vsplit
+          opts = {
+            breakindent = true,
+            cursorcolumn = false,
+            cursorline = false,
+            foldcolumn = '0',
+            linebreak = true,
+            list = false,
+            numberwidth = 1,
+            signcolumn = 'no',
+            spell = false,
+            wrap = true,
+          },
+        },
+
+        ---Customize how tokens are displayed
+        ---@param tokens number
+        ---@param adapter CodeCompanion.Adapter
+        ---@return string
+        token_count = function(tokens, adapter)
+          return ' (' .. tokens .. ' tokens)'
+        end,
+      },
     },
     opts = {
-      log_level = 'ERROR', -- TRACE|DEBUG|ERROR|INFO
+      log_level = 'INFO', -- TRACE|DEBUG|ERROR|INFO
       language = 'English',
       system_prompt = function(_opts)
         return require('dcai.llm.prompt_library').BASE_PROMPT_CODING
