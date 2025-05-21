31dba8ee2f91f489cadd9488b1419c9bba92b46f
dcai <d@tux.im>
2025-05-21 15:17:50 +1000

    feat(lsp): Add workspace symbols and rebind format keymap
    This commit introduces a new keymap `<leader>ll` for listing workspace symbols using fzf-lua.
    It also rebinds the format keymap from `<leader>lf` to a function that sets the quickfix list and then calls `fzf.lsp_live_workspace_symbols()`.
    The original format keymap is commented out.

--- lua/dcai/keymaps/lsp.lua
+++ lua/dcai/keymaps/lsp.lua
@@ -7,12 +7,25 @@ local lsp_keymap = {
   -- grr is default references
   -- gri is default find implementation
   { '<leader>ld', vim.diagnostic.open_float, desc = 'diagnostic' },
-  { '<leader>lf', vim.lsp.buf.format, desc = 'format code' },
+  -- { '<leader>lf', vim.lsp.buf.format, desc = 'format code' },
   { '<leader>lh', vim.lsp.buf.hover, desc = 'lsp hover' },
   { '<leader>lI', '<cmd>checkhealth lsp<cr>', desc = 'lsp info' },
   { '<leader>li', '<cmd>Inspect<cr>', desc = 'inspect' },
   {
     '<leader>ll',
+    function()
+      vim.diagnostic.setqflist({
+        open = false,
+      })
+      local fzf = require('fzf-lua')
+      -- lsp_live_workspace_symbols executes an LSP request on each key press.
+      -- lsp_workspace_symbols executes an empty LSP request the first time and then uses fzf to filter the results.
+      fzf.lsp_live_workspace_symbols()
+    end,
+    desc = 'list workspace symbols',
+  },
+  {
+    '<leader>lf',
     function()
       vim.diagnostic.setqflist({
         open = false,
