824cb5cc054c0998d3f7cc48e61e2c846093ed8e
dcai <d@tux.im>
2025-12-07 13:27:07 +1100

    chore(plug): add commented options for copilot npx and node command configuration

--- lua/dcai/plug.lua
+++ lua/dcai/plug.lua
@@ -52,6 +52,8 @@ M.setup = function(plug_opts)
     setup = function()
       vim.g.copilot_no_tab_map = true
       vim.g.copilot_proxy_strict_ssl = false
+      -- vim.g.copilot_npx = true
+      -- vim.gcopilot_node_command = '~/.nodenv/versions/24.00.0/bin/node'
       vim.keymap.set('i', '<C-F>', 'copilot#Accept("\\<CR>")', {
         expr = true,
         replace_keycodes = false,
