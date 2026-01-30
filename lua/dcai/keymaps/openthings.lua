51d921fb625305305986ec1de26db84379113998
dcai <d@tux.im>
2026-01-30 23:13:02 +1100

    fix(keymaps): update Sublime Text path string to use Lua long brackets for consistency

--- lua/dcai/keymaps/openthings.lua
+++ lua/dcai/keymaps/openthings.lua
@@ -1,6 +1,6 @@
 local utils = require('dcai.keymaps.utils')
 
-local subl = '/Applications/Sublime\\ Text.app/Contents/SharedSupport/bin/subl'
+local subl = [[/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl]]
 -- local zed = [[/Applications/Zed.app/Contents/MacOS/cli]]
 
 local openthings_keymap = {
