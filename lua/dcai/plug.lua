0498e13e2eec31e98d8f75a254ed59f877d062a5
dcai <d@tux.im>
2026-08-10 16:09:49 +1000

    chore(plug): use npm to install markdown preview dependencies

--- lua/dcai/plug.lua
+++ lua/dcai/plug.lua
@@ -178,7 +178,7 @@ M.setup = function(plug_opts)
     end,
   })
   Plug('dcai/markdown-preview.nvim', {
-    ['do'] = 'cd app && npx --yes yarn install',
+    ['do'] = 'cd app && npm install',
     ['for'] = 'markdown',
     setup = function()
       vim.g.mkdp_theme = 'light'
